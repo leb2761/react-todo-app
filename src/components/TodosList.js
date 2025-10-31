9098476590240c1b802b41d6e8124a122f91131a
Brandan Le <le.brandan5@gmail.com>
2025-10-31 16:45:56 -0400

    Initial commit - React ToDo App

---
+++ src/components/TodosList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+class TodosList extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul style={{ paddingLeft: 0 }}>
+          {this.props.todos.map(todo => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              handleChangeProps={this.props.handleChangeProps}
+              deleteTodoProps={this.props.deleteTodoProps}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodosList;
